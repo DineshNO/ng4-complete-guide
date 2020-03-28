4894351147539b56e6219a4df719f2cd848a27a5
Dinesh Kumar <ldineshkumar85@gmail.com>
2020-03-28 17:59:20 +0100

    NgRx :  Header component changed to use auth reducer

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from "@angular/core";
 import * as firebase from 'firebase';
 import { ActivatedRoute, Router } from "@angular/router";
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.action'
+import { Store } from "@ngrx/store";
 
 @Injectable()
 export class Authservice {
     token: string;
     constructor(private route: ActivatedRoute,
-        private router: Router) { }
+        private router: Router,
+        private store : Store<fromApp.AppState>) { }
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(
+                user =>  this.store.dispatch(new AuthActions.Signup())
+            )
             .catch(
                 error => console.log(error)
             );
@@ -19,6 +26,7 @@ export class Authservice {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    this.store.dispatch(new AuthActions.Signin())
                     firebase.auth().currentUser.getIdToken()
                         .then(
                             (token: string) => this.token = token
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,32 +1,46 @@
-import { Component } from "@angular/core";
 import { HttpEvent } from "@angular/common/http";
-import { Authservice } from "../../auth/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 import { DataStorageService } from "../../shared/data-storage.service";
+import * as fromApp from '../../store/app.reducer';
+import * as fromAuth from '../../auth/store/auth.reducer';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
+    authState: Observable<fromAuth.State>;
 
-    constructor(private dataStorage: DataStorageService,
-        public authService : Authservice) { }
+    constructor(private dataStorage: DataStorageService, public store: Store<fromApp.AppState>) { }
+
+    ngOnInit(){
+        this.authState = this.store.select('auth');          
+        this.store.select('auth')
+            .subscribe(
+                (data) => console.log("Authenticate :::::::::",data.authenticated)
+            )                ;          
+
+    }
+
     onSaveData() {
         this.dataStorage.storeRecipe()
             .subscribe(
-                (response: HttpEvent<Object>) => { 
+                (response: HttpEvent<Object>) => {
                     console.log(response);
                 }
             );
     }
 
-    onFetchData(){
+    onFetchData() {
         this.dataStorage.getRecipes();
     }
 
-    onLogout(){
-        this.authService.signOut();
+    onLogout() {
+        //this.authService.signOut();
     }
 
 }
